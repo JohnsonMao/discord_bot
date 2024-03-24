c6d74826b3d63b6935ed81d5d6b776bf33e9a2b4
Johnson Mao <tutelary.maomao@gmail.com>
2024-03-24 20:22:59 +0800

    chore: add test env

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from "vite";
-import path from "path";
-import react from "@vitejs/plugin-react";
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -11,8 +12,25 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "~": path.resolve(__dirname, "./"),
+      '@': path.resolve(__dirname, './src'),
+      '~': path.resolve(__dirname, './'),
+    },
+  },
+  test: {
+    watch: false,
+    include: ['**/*.test.{ts,jsx}'],
+    setupFiles: ['./tests/vitest.setup.js'],
+    environment: 'jsdom',
+  },
+  server: {
+    open: true,
+    host: '0.0.0.0',
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
 });
