ea1bf54ee03a5ce8047ade84176fe6c908b4007a
Johnson Mao <tutelary.maomao@gmail.com>
2024-03-30 20:01:44 +0800

    refactor: i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,13 +1,17 @@
-import i18n from "i18next";
-import Backend from "i18next-http-backend";
-import LanguageDetector from "i18next-browser-languagedetector";
-import { initReactI18next } from "react-i18next";
+import i18n from 'i18next';
+import Backend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
 
-export const fallbackLng = "en";
+export const fallbackLng = 'en';
 
-i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
-  fallbackLng,
-  debug: true,
-});
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng,
+    debug: import.meta.env.DEV,
+  });
 
 export default i18n;
